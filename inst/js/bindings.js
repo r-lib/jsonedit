6ccf65900cf8e0b8fcd07736d880000105e7dc2d
Davis Vaughan <davis@posit.co>
2025-08-06 10:57:25 -0400

    Fail in the face of parse errors (#1)

--- inst/js/bindings.js
+++ inst/js/bindings.js
@@ -7,3 +7,9 @@ function json_format(json, opts){
   var edit_result = jsonc.format(json, undefined, opts);
   return jsonc.applyEdits(json, edit_result);
 }
+
+function json_parse_errors(json){
+  const errors = [];
+  jsonc.parseTree(json, errors);
+  return errors;
+}
